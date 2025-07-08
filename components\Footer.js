4254591ba9bcc45c57db92af8d2eb57e45b3cc70
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:23:13 +0500

    Add components\Footer.js

---
+++ components\Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 py-4">
+      <div className="container">
+        <p className="text-lg font-bold">Copyright 2023 Argentina Football Team</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
