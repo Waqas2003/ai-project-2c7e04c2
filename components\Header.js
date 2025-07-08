6bb6727b4985f52f92a7fe4f70491b0f1c34fe60
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:23:15 +0500

    Add components\Header.js

---
+++ components\Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-white shadow-md py-4">
+      <div className="container">
+        <h1 className="text-3xl font-bold">Argentina Football Team</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
